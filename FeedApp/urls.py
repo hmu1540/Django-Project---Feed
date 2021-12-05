d97f9f7356dfc597d0f2a665a0230b189c36b855
Huimin <huimin_mu1@baylor.edu>
2021-12-04 19:28:58 -0600

    Build the profile page

---
+++ FeedApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'FeedApp'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
+    path('myfeed', views.myfeed, name='myfeed'),
+
+]
--- FeedApp/views.py
+++ FeedApp/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm, ProfileForm, RelationshipForm
+from .models import Post, Comment, Like, Profile, Relationship
+from datetime import datetime, date
+
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 
 # Create your views here.
+
+# When a URL request matches the pattern we just defined, 
+# Django looks for a function called index() in the views.py file.
+
+def index(request):
+    """ The home pate for FeedApp. """
+    return render(request, 'FeedApp/index.html')
+
+
+
+@login_required #only after logging in, can access the profile funciton(protection)
+def profile(request):
+    profile = Profile.objects.filter(user=request.user) # only user that logged on can access information of their own.
+    if not profile.exists():
+        Profile.objects.create(user=request.user)
+    profile = Profile.objects.get(user=request.user) # can use get() only when we are sure the user exists
+
+    if request.method != 'POST':
+        form =ProfileForm(instance=profile) 
+    else:
+        form = ProfileForm(instance=profile,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('FeedApp:profile')
+        
+    context = {'form':form}
+    return render(request,'FeedApp/profile.html',context)
+
+@login_required
+def myfeed(request):    
+    comment_count_list = []
+    like_count_list = []
+    posts = Post.objects.filter(username=request.user).order_by('-date_posted') #username is a field of Post
+    

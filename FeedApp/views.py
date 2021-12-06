5b82b402c55ff38a1685adaa31bb5f221bd3641f
Huimin <huimin_mu1@baylor.edu>
2021-12-05 22:08:04 -0600

    Create the page myfeed

--- FeedApp/views.py
+++ FeedApp/views.py
@@ -36,9 +36,74 @@ def profile(request):
     context = {'form':form}
     return render(request,'FeedApp/profile.html',context)
 
+
 @login_required
 def myfeed(request):    
     comment_count_list = []
     like_count_list = []
     posts = Post.objects.filter(username=request.user).order_by('-date_posted') #username is a field of Post
+    for p in posts:
+        c_count = Comment.objects.filter(post=p).count()
+        l_count = Like.objects.filter(post=p).count()
+        comment_count_list.append(c_count)
+        like_count_list.append(l_count)
+    zipped_list = zip(posts, comment_count_list, like_count_list)    #pass post,comment count,like count to context or zip all up so that they are arranged in a 'dataframe' way
     
+    context = {'posts':posts, 'zipped_list':zipped_list}
+    return render(request, 'FeedApp/myfeed.html', context)
+
+
+
+@login_required
+def new_post(request):
+    if request.method != 'POST':
+        form = PostForm()
+    else:
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.username = request.user
+            new_post.save()
+            return redirect('FeedApp:myfeed')
+
+    context= {'form':form}
+    return render(request, 'FeedApp/new_post.html', context) 
+
+
+
+@login_required
+def comments(request, post_id):
+    if request == 'POST' and request.POST.get('btn1'):
+        text = request.POST.get("comment")
+        Comment.objects.create(post_id=post_id,username=request.user,text=text,date_added=date.today())
+
+    comments = Comment.objects.filter(post=post_id)
+    post = Post.objects.get(id=post_id)
+
+    context ={'comments':comments, 'post':post}
+
+    return render(request, 'FeedApp/comments.html', context)
+
+@login_required
+def friendsfeed(request):    
+    comment_count_list = []
+    like_count_list = []
+    posts = Post.objects.filter(username=request.user).order_by('-date_posted') #username is a field of Post
+    for p in posts:
+        c_count = Comment.objects.filter(post=p).count()
+        l_count = Like.objects.filter(post=p).count()
+        comment_count_list.append(c_count)
+        like_count_list.append(l_count)
+    zipped_list = zip(posts, comment_count_list, like_count_list)    #pass post,comment count,like count to context or zip all up so that they are arranged in a 'dataframe' way
+    
+    if request.method == 'POST' and request.POST.get('like'):
+        post_to_like = request.POST.get('like')
+        print(post_to_like)
+        like_already_exists = Like.objects.filter(post_id=post_to_like,username=request.user)
+        if not like_already_exists:
+            Like.objects.create(post_id=post_to_like,username=request.user)
+            return redirect('FeedApp:friendsfeed')
+
+
+    context = {'posts':posts, 'zipped_list':zipped_list}
+    return render(request, 'FeedApp/myfeed.html', context)
